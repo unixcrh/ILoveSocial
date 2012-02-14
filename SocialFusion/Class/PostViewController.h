d0a85fbd76c50f3c1b37af55c077a48df30b4901
bluebitch <wzc345@gmail.com>
2012-02-14 21:09:38 +0800

    准备开始写留言和回复的vc。

---
+++ SocialFusion/Class/PostViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostViewController.h
+//  SocialFusion
+//
+//  Created by Blue Bitch on 12-2-14.
+//  Copyright (c) 2012年 TJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostViewController : UIViewController
+
+@end
