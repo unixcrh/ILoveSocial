cd7730e40456741c8f96f5fc5d6c3947cd366909
bluebitch <wzc345@gmail.com>
2012-02-27 00:49:15 +0800

    各种Merge.

---
+++ SocialFusion/Class/NewBlogViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewBlogViewController.h
+//  SocialFusion
+//
+//  Created by He Ruoyun on 12-2-26.
+//  Copyright (c) 2012年 TJU. All rights reserved.
+//
+
+#import "NewStatusViewController.h"
+
+@interface NewBlogViewController : NewStatusViewController
+
+@end
