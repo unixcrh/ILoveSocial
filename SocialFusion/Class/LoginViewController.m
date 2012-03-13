3a32ea49dfa0f72d8f48b37a46c8eddc9b1dd8ee
bluebitch <wzc345@gmail.com>
2012-03-13 19:00:09 +0800

    解决登陆微博时人人的头像会闪的问题。

--- SocialFusion/Class/LoginViewController.m
+++ SocialFusion/Class/LoginViewController.m
@@ -231,7 +231,7 @@
     Image *image = [Image imageWithURL:self.currentWeiboUser.tinyURL inManagedObjectContext:self.managedObjectContext];
     if (image == nil) {
         [self.weiboPhotoImageView loadImageFromURL:self.currentWeiboUser.tinyURL completion:^{
-            [self.renrenPhotoImageView fadeIn];
+            [self.weiboPhotoImageView fadeIn];
         } cacheInContext:self.managedObjectContext];
     }
     else {
